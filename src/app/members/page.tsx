9197cd558b39d8367e48a7296fefd5d03827f352
Piotr Muszel <piotr.muszel@supremis.pl>
2024-08-27 09:46:39 +0200

    End of section 5

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -2,15 +2,17 @@ import Link from 'next/link'
 import React from 'react'
 import { getMembers } from '../actions/memberActions'
 import MemberCard from './MemberCard';
+import { fetchCurrentUserLikeIds } from '../actions/likeActions';
 
 export default async function MembersPage() {
 
   const members = await getMembers();
+  const likeIds = await fetchCurrentUserLikeIds();
 
   return (
-    <div className='mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-8'>      
+    <div className='mt-10 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-8'>      
           {members && members.map(m => (
-            <MemberCard key={m.id} member={m}/>
+            <MemberCard key={m.id} member={m} likeIds={likeIds}/>
           ))}       
     </div>
   )
